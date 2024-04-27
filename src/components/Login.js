98bdefdd454743644728c812c48524dc45d4663e
Shashwat Sahu <shashwatsahu780@gmail.com>
2024-04-27 22:43:15 +0530

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,104 +1,116 @@
-import React, { useState,useRef } from 'react'
-import Header from './Header'
-import { checkvlaidData } from '../utils/validate'
-import { auth } from '../utils/firebase'
-import { createUserWithEmailAndPassword , signInWithEmailAndPassword} from "firebase/auth";
+import { useState, useRef } from "react";
+import Header from "./Header";
+import { checkValidData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+
+
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-    const [isSignInForm,setisSignInForm]=useState(true)
-    const [Errormessage,setErrormessage]=useState(null)
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
 
-    const email=useRef(null)
-    const password=useRef(null) // here email and password.. are two Object which is created by useRef hook
-    const username=useRef(null)
-    const phonenum=useRef(null)
-    const handlebuttonclick =()=>{    
-     const message= checkvlaidData(email.current.value,password.current.value,username.current.value,phonenum.current.value) //these are the value inside he objects
-     setErrormessage(message)
-     if(message) return   // if there is an error it will return if it is correct then go for signin/signup
+  const handleButtonClick = () => {
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+    if (message) return;
 
-        //sign in sign Up logic    
-     if(!isSignInForm){
-          //sign up logic
-          createUserWithEmailAndPassword(auth, email.current.value,password.current.value)
-              .then((userCredential) => {
-                // Signed up 
-                const user = userCredential.user;
-                console.log(user)
-                // ...
-              })
-              .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // ..
-                setErrormessage(errorCode+'-'+errorMessage)
-              });
-     }
-     else{
-          //sign in logic
-          signInWithEmailAndPassword(auth, email.current.value, password.current.value )
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user)
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    setErrormessage(errorCode+'-'+errorMessage)
-  });
-     }
+    if (!isSignInForm) {
+      // Sign Up Logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        // ...
+        console.log(user)
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setErrorMessage(errorCode+' '+errorMessage)
+        // ..
+      });
+    } else {
+      // Sign In Logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user)
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
     }
+  };
 
-    const toggleSigninFrom =()=>{
-         setisSignInForm(!isSignInForm)
-    }
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
-        <Header/>
-        <div className='absolute'>
-            <img src='https://assets.nflxext.com/ffe/siteui/vlv3/7ca5b7c7-20aa-42a8-a278-f801b0d65fa1/fb548c0a-8582-43c5-9fba-cd98bf27452f/IN-en-20240326-popsignuptwoweeks-perspective_alpha_website_small.jpg' alt='Background'/>
-        </div>
-        <form onSubmit={(e)=>e.preventDefault()} className='w-3/12 absolute p-8 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-80'>
-             <h1 className='font-bold text-3xl py-4'>{isSignInForm ? 'Sign In':'Sign Up'}</h1>
+      <Header />
+      <div className="absolute">
+        <img className="h-screen object-cover w-screen " src='https://assets.nflxext.com/ffe/siteui/vlv3/7ca5b7c7-20aa-42a8-a278-f801b0d65fa1/fb548c0a-8582-43c5-9fba-cd98bf27452f/IN-en-20240326-popsignuptwoweeks-perspective_alpha_website_small.jpg' alt="logo" />
+      </div>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-full md:w-3/12  absolute p-8 bg-black my-24 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
 
-             {!isSignInForm && (
-            <div>
-             <input 
-             ref={username}
-             type='text'
-             placeholder='Name'
-             className='p-2 my-3 w-full bg-slate-900'
-             /> 
-             <input
-             ref={phonenum} 
-             type='text'
-             placeholder='Phon number'
-             className='p-2 my-3 w-full bg-slate-900'
-             />
-            </div>
-             )}
-             <input 
-             ref={email}
-             type='text'
-             placeholder='Email Address'
-             className='p-2 my-3 w-full bg-slate-900'
-             />
-             <input 
-             ref={password}
-             type='text'
-             placeholder='Password'
-             className='p-2 my-3 w-full bg-slate-900'
-             />
-            <p className='text-red-600 font-bold'>{Errormessage}</p>
-            <button className='p-2 my-6 bg-red-600 w-full rounded-lg' onClick={handlebuttonclick}>
-              {isSignInForm ? 'Sign In':'Sign Up'}
-            </button>
-            <p className='py-4 cursor-pointer' onClick={toggleSigninFrom}>{isSignInForm ? 'New to Netflix? Sign Up Now':'Alredy user ? Sign In Now'}</p>
-        </form>
+        {!isSignInForm && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-2 my-2 w-full bg-gray-900"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email Address"
+          className="p-2 my-2 w-full bg-gray-900"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-2 my-2 w-full bg-gray-900"
+        />
+        <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+        <button
+          className="p-2 my-4 bg-red-700 w-full rounded-lg"
+          onClick={handleButtonClick}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now."}
+        </p>
+      </form>
     </div>
-  )
-}
-
-export default Login
+  );
+};
+export default Login;
